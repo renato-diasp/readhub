9e0df82dca02bc9acdd6cc43789d41a4b97a8f28
Gabriela Toledo <gabrielapdmtoledo@gmail.com>
2024-05-09 21:19:11 -0300

    criando o botão switch do index

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
+// função para o botão switch do header
+let switchState = false;
 
+function toggleSwitch() {
+  switchState = !switchState;
+  const switchButton = document.getElementById('switchButton');
+
+  if (switchState) {
+    switchButton.classList.remove('off');
+    switchButton.classList.add('on');
+    // Redireciona para outra URL quando o botão é ligado
+    window.location.href = './indexReviews.html';
+  } else {
+    switchButton.classList.remove('on');
+    switchButton.classList.add('off');
+    // Redireciona para outra URL quando o botão é desligado
+    window.location.href = "./index.html" ;
+  }
+}
