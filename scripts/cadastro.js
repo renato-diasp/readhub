616a0cfdbc09591425dff9fbd13f1744f6c5cc10
Gustavo Jun <gustavojunmoritani@gmail.com>
2024-05-27 11:15:59 -0300

    comentários no código

--- scripts/cadastro.js
+++ scripts/cadastro.js
@@ -1,19 +1,24 @@
 function handleSignup(event) {
   event.preventDefault();
 
+  // acesso as infos do form
   var email = document.getElementById("email").value;
   var username = document.getElementById("username").value;
   var password = document.getElementById("password").value;
   var confirmPassword = document.getElementById("confirmPassword").value;
 
+  // acesso a uma div do html que gerará uma resposta em innerHTML de sucesso de cadastro ou nao para o usuário
   var alert = document.getElementById("alert");
 
+  // acesso das infos do local storage
   var usersStorage = JSON.parse(localStorage.getItem("users")) || [];
 
+  // checa se email já foi cadastrado
   var checkEmail = usersStorage.find(function (userLogin) {
     return userLogin.email === email;
   });
 
+  // checa se usuário já foi cadastrado
   var checkUsername = usersStorage.find(function (userLogin) {
     return userLogin.username === username;
   });
@@ -28,17 +33,21 @@ function handleSignup(event) {
     return;
   }
 
+  // checa se as senhas são iguais
   if (password !== confirmPassword) {
     alert.innerHTML = "<p> As senhas não são iguais </p>";
     return;
   }
 
+  // objeto padrão de usuário
   var user = {
     email: email,
     username: username,
     password: password,
+    wishlist: [],
   };
 
+  // armazena informações no local storage
   try {
     usersStorage.push(user);
     localStorage.setItem("users", JSON.stringify(usersStorage));
--- scripts/login.js
+++ scripts/login.js
@@ -1,30 +1,35 @@
 function handleLogin(event) {
   event.preventDefault();
 
+  // pega as infos do form
   var loginInput = document.getElementById("loginInput").value;
   var passwordInput = document.getElementById("passwordInput").value;
 
+  // acesso das infos do local storage
   var usersStorage = JSON.parse(localStorage.getItem("users")) || [];
 
+  // acesso a uma div do html que gerará uma resposta em innerHTML de sucesso de login ou nao para o usuário
   var alert = document.getElementById("alert");
 
+  // busca do usuário no local storage
   var foundUser = usersStorage.find(function (userLogin) {
     return userLogin.email === loginInput || userLogin.username === loginInput;
   });
 
+  // checa se a senha está correta e efetua o login
   if (foundUser && foundUser.password === passwordInput) {
-    // alert("Login efetuado.");
-
+    // adiciona o usuário que fez o login em uma outra chave do local sotorage
     localStorage.removeItem("loggedInUser");
     localStorage.setItem(
       "loggedInUser",
       JSON.stringify({ email: foundUser.email })
     );
-
+    // vai para a página de login
     window.location.href = "profile.html";
   } else {
     alert.innerHTML = "<p> Usuário ou senha incorretos </p>";
   }
 }
 
+// Executa a função após clicar em login
 document.getElementById("loginButton").addEventListener("click", handleLogin);
